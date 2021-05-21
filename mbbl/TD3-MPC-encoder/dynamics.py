0d20f3b3d1d4d119b3203fc292bdbb7b487253e1
Utkarshmishra04 <utkarsh75477@gmail.com>
2021-05-21 16:26:19 +0530

    refactored

--- mbbl/TD3-MPC-encoder/dynamics.py
+++ mbbl/TD3-MPC-encoder/dynamics.py
@@ -121,47 +121,47 @@ class Encoder():
         self.encoder = MLPnetwork1(self.state_dim, self.latent_dim, dim_hidden=128).to(device)
         self.decoder = MLPnetwork1(self.latent_dim, self.state_dim, dim_hidden=128).to(device)
         self.forward_dynamics = GaussianModel2(self.latent_dim, self.action_dim, self.latent_dim, dim_hidden=128).to(device)
-        self.inverse_dynamics = GaussianModel2(self.latent_dim, self.latent_dim, self.action_dim, dim_hidden=128).to(device)
+        # self.inverse_dynamics = GaussianModel2(self.latent_dim, self.latent_dim, self.action_dim, dim_hidden=128).to(device)
 
         # self.optimizer = optim.Adam(list(self.encoder.parameters())+list(self.decoder.parameters()), lr=0.0003)
         self.optimizer_encoder = optim.Adam(self.encoder.parameters(), lr=0.0003)
         self.optimizer_decoder = optim.Adam(self.decoder.parameters(), lr=0.0003)
         self.optimizer_fdyn = optim.Adam(self.forward_dynamics.parameters(), lr=0.0003)
-        self.optimizer_idyn = optim.Adam(self.inverse_dynamics.parameters(), lr=0.0003)
+        # self.optimizer_idyn = optim.Adam(self.inverse_dynamics.parameters(), lr=0.0003)
 
         self.loss = 0
         self.fdyn_loss = 0
-        self.idyn_loss = 0
+        # self.idyn_loss = 0
 
     def update_encoder(self, state1, action1, reward1, next_state1, state2, action2, reward2, next_state2):
 
         encoded_state1 = self.encoder.forward(state1)
         encoded_nstate1 = self.encoder.forward(next_state1)
         pred_encoded_nstate1 = self.forward_dynamics.sample_prediction(encoded_state1, action1)
-        pred_encoded_action1 = self.inverse_dynamics.sample_prediction(encoded_state1, pred_encoded_nstate1)
+        # pred_encoded_action1 = self.inverse_dynamics.sample_prediction(encoded_state1, pred_encoded_nstate1)
         decoded_state1 = self.decoder.forward(pred_encoded_nstate1)
         decoded_nstate1 = self.decoder.forward(pred_encoded_nstate1)
         
         encoded_state2 = self.encoder.forward(state2)
         encoded_nstate2 = self.encoder.forward(next_state2)
         pred_encoded_nstate2 = self.forward_dynamics.sample_prediction(encoded_state2, action2)
-        pred_encoded_action2 = self.inverse_dynamics.sample_prediction(encoded_state2, pred_encoded_nstate2)
+        # pred_encoded_action2 = self.inverse_dynamics.sample_prediction(encoded_state2, pred_encoded_nstate2)
         decoded_state2 = self.decoder.forward(pred_encoded_nstate2)
         decoded_nstate2 = self.decoder.forward(pred_encoded_nstate2)
                 
         self.fdyn_loss = nn.MSELoss()(pred_encoded_nstate1, encoded_nstate1) + nn.MSELoss()(pred_encoded_nstate2, encoded_nstate2)
-        self.idyn_loss = nn.MSELoss()(pred_encoded_action1, action1) + nn.MSELoss()(pred_encoded_action2, action2)   
-        self.loss = nn.MSELoss()(state1,decoded_state1)+nn.MSELoss()(next_state1,decoded_nstate1)+self.fdyn_loss+self.idyn_loss
+        # self.idyn_loss = nn.MSELoss()(pred_encoded_action1, action1) + nn.MSELoss()(pred_encoded_action2, action2)   
+        self.loss = nn.MSELoss()(state1,decoded_state1)+nn.MSELoss()(next_state1,decoded_nstate1)+self.fdyn_loss #+self.idyn_loss
         
         self.optimizer_fdyn.zero_grad()
-        self.optimizer_idyn.zero_grad()
+        # self.optimizer_idyn.zero_grad()
         self.optimizer_decoder.zero_grad()
         self.optimizer_encoder.zero_grad()
 
         self.loss.backward()   
 
         self.optimizer_fdyn.step()
-        self.optimizer_idyn.step()
+        # self.optimizer_idyn.step()
         self.optimizer_decoder.step()
         self.optimizer_encoder.step()
         
@@ -180,12 +180,12 @@ class Encoder():
         # deco_loss = torch.mean(self.decoder_loss)
         loss = torch.mean(self.loss)
         fd_loss = torch.mean(self.fdyn_loss)
-        id_loss = torch.mean(self.idyn_loss)
+        # id_loss = torch.mean(self.idyn_loss)
 
         writer.add_scalar("encodings/module_loss", loss, i_iter)
         # writer.add_scalar("encodings/decoder_loss", enco_loss, i_iter)
         writer.add_scalar("encodings/forward_dynamics_loss", fd_loss, i_iter)
-        writer.add_scalar("encodings/inverse_dynamics_loss", id_loss, i_iter)
+        # writer.add_scalar("encodings/inverse_dynamics_loss", id_loss, i_iter)
 
         return writer
 
